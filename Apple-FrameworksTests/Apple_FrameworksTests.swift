7d1608936a5dcd9c729ebcf7ed7797feab553213
tinajuresko <tina.juresko16@gmail.com>
2024-09-27 17:30:20 +0200

    Initial Commit

---
+++ Apple-FrameworksTests/Apple_FrameworksTests.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksTests.swift
+//  Apple-FrameworksTests
+//
+//  Created by Tina Jureško on 27.09.2024..
+//
+
+import Testing
+@testable import Apple_Frameworks
+
+struct Apple_FrameworksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
